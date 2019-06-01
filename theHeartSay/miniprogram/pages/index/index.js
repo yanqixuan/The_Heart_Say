60569ba7ccb8f3f61ab3ecca358af60abd6fd572
yanqixuan <1402412256@qq.com>
2019-06-01 21:29:36 +0800

    至云数据库

--- theHeartSay/miniprogram/pages/index/index.js
+++ theHeartSay/miniprogram/pages/index/index.js
@@ -1,9 +1,67 @@
 const app = getApp()
-Page({
-  data:{
+const db = wx.cloud.database({
+  env:'http-http'
+});
+const _ =db.command
 
+Page({
+    data:{
+      cartList:[]
+  },
+  changeActive(e){
+    let active = e.detail.active;
+    let id = e.detail.id;
+    console.log('changeactive',active,id)
+    return db.collection('starBucksCard').doc(e.detail.id).update({
+      data:{
+        like: false
+      },
+      success: res => {
+        console.error('[数据库] [更新记录] ：', res)
+      },
+      fail: err => {
+        console.error('[数据库] [更新记录] 失败：', err)
+    }
+  })
+    // console.log(selectDB)
+    // selectDB.get({
+    //   success(res){
+    //     console.log(res)
+    //   }
+    // })
+    // selectDB.update({
+    //   data:{
+    //     test:'修改后的值'
+    //   },
+    // })
+  },
+  likePic(e){
+    console.log('likepic')
   },
   onLoad(){
-    
+   wx.request({
+     url: 'https://www.easy-mock.com/mock/5cf1f4eaabb0047e81554dd5/starbucks/collectionScroll',
+     data: {},
+     header: {'content-type':'application/json'},
+     method: 'GET',
+     dataType: 'json',
+     responseType: 'text',
+     success: (result)=>{
+      //  console.log(result);
+        this.setData({
+          cartList:result.data.data.cartList
+        })
+     },
+     fail: ()=>{},
+     complete: ()=>{}
+   }); 
+
+  //  数据库查询
+  db.collection('starBucksCard').where({
+  }).get({
+    success(res){
+      // console.log(res)
+    }
+  })
   }
 })
