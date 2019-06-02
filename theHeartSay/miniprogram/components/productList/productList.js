c95f2e3c6a4b05352d4942acd43404c1adde8bd8
7287vencent <1992630314@qq.com>
2019-06-02 09:04:03 +0800

    product

---
+++ theHeartSay/miniprogram/components/productList/productList.js
@@ -0,0 +1,82 @@
+// components/productList/productList.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    productList: {
+      type: Object,
+      value: {}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    status: false,
+    listNum: 0,
+    id: '0'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  lifetimes: {
+   ready() {
+    // const id = this.properties.productList.id
+    // console.log(this.properties.productList)
+    // this.setData({
+    //   id
+    // })
+    // console.log(id)
+    }
+  },
+  methods: {
+    addList () {
+      let status = !this.data.status
+      let listNum = this.data.listNum
+      this.setData({
+        status,
+        listNum: ++listNum
+      })
+      this.getListNum()
+    },
+    addNum () {
+      let listNum = this.data.listNum
+      listNum++
+      this.setData({
+        listNum
+      })
+      this.getListNum()
+    },
+    subtractNum () {
+      let listNum = this.data.listNum
+      // setTimeout(()=>{},500)
+      if(listNum <= 1) {
+        // console.log('222')
+        let status = !this.data.status
+        listNum--
+        this.setData({
+          status,
+          listNum
+        })
+        this.getListNum()
+        return
+      }
+      // console.log('111')
+      listNum--
+      this.setData({
+        listNum
+      })
+      this.getListNum()
+    },
+    getListNum () {
+      // return this.data.listNum
+      this.triggerEvent('getListNum', {num: this.data.listNum,
+      id: this.properties.productList.id,
+      price: this.properties.productList.price
+    })
+    }
+  }
+})
--- theHeartSay/miniprogram/pages/index/index.js
+++ theHeartSay/miniprogram/pages/index/index.js
@@ -1,9 +1,73 @@
 const app = getApp()
+let dataMap = new Map()
 Page({
   data:{
-
+    productList: {
+      // id: '3',
+      // img: "http://www.ylwgift.com/Files/Product/1473/201503200455025082-1.jpg",
+      // title: '星冰粽冰尚礼袋',
+      // price: '198'
+    },
+    productLists: [],
+    buyNum: 0,
+    buyAllPrice: '0.00',
+    showModal: false
   },
   onLoad(){
-    
+    this.getProductData()
+  },
+  getProductData () {
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5cf1f4eaabb0047e81554dd5/starbucks/productList#!method=get',
+      header: {'content-type':'application/json'},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (res) => {
+        // console.log(res)
+        this.setData({
+          productLists: res.data.data.productList
+        })
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+      
+  },
+  getListNum(e) {
+    // console.log(e)
+    // console.log(e.detail)
+    let id = e.detail.id
+    let num = e.detail.num
+    let price = e.detail.price
+    dataMap.set(id,{num,price})
+    // console.log(dataMap)
+    this.getBuyNum()
+  },
+  getBuyNum () {
+    const values =Array.from(dataMap.values()) 
+    // console.log(values)
+    let buyNum = 0,
+    buyAllPrice = 0
+    for(let item of values) {
+      buyNum += item.num
+      buyAllPrice += item.price * item.num
+    }
+    // console.log(num,price)
+    // buyAllPrice
+    this.setData({
+      buyNum,
+      buyAllPrice: buyAllPrice.toFixed(2)
+    })
+  },
+  seeDetail(e) {
+    // e.stopImmediatePropagation()
+    const productList = e.target.dataset.item
+    this.setData({
+      showModal: true,
+      productList
+    })
+    // console.log(productList)
+    // e.stopPropagation()
   }
 })
