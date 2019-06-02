9f25833df062ab446ff6cfaf909fd69ffc953d08
7287vencent <1992630314@qq.com>
2019-06-02 09:18:57 +0800

    整合HY

--- theHeartSay/miniprogram/pages/detail/detail.js
+++ theHeartSay/miniprogram/pages/detail/detail.js
@@ -1,20 +1,81 @@
+const app = getApp()
+const db = wx.cloud.database({
+  env:'http-http'
+});
+
+const _ =db.command
+
+
 let dataMap = new Map()
 Page({
   data:{
-    productList: {
-      // id: '3',
-      // img: "http://www.ylwgift.com/Files/Product/1473/201503200455025082-1.jpg",
-      // title: '星冰粽冰尚礼袋',
-      // price: '198'
-    },
+    productList: {},
     productLists: [],
     buyNum: 0,
     buyAllPrice: '0.00',
-    showModal: false
+    showModal: false,
+    cartList:[]
   },
   onLoad(){
     this.getProductData()
+// 鄢启轩写的
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5cf1f4eaabb0047e81554dd5/starbucks/collectionScroll',
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (result) => {
+        //  console.log(result);
+        this.setData({
+          cartList: result.data.data.cartList
+        })
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+
+    //  数据库查询
+    db.collection('starBucksCard').where({}).get({
+      success(res) {
+        // console.log(res)
+      }
+    })
   },
+  changeActive(e){
+    let active = e.detail.active;
+    let id = e.detail.id;
+    console.log('changeactive',active,id)
+    return db.collection('starBucksCard').doc(e.detail.id).update({
+      data:{
+        like: false
+      },
+      success: res => {
+        console.error('[数据库] [更新记录] ：', res)
+      },
+      fail: err => {
+        console.error('[数据库] [更新记录] 失败：', err)
+    }
+  })
+    // console.log(selectDB)
+    // selectDB.get({
+    //   success(res){
+    //     console.log(res)
+    //   }
+    // })
+    // selectDB.update({
+    //   data:{
+    //     test:'修改后的值'
+    //   },
+    // })
+  },
+  likePic(e){
+    console.log('likepic')
+  },
+  // 贺海写的
   getProductData () {
     wx.request({
       url: 'https://www.easy-mock.com/mock/5cf1f4eaabb0047e81554dd5/starbucks/productList#!method=get',
