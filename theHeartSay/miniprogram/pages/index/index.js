36ebfa746b43b9e504447e439f58fc0144299b26
unknown <1356452262@qq.com>
2019-06-02 09:22:21 +0800

    bin

--- theHeartSay/miniprogram/pages/index/index.js
+++ theHeartSay/miniprogram/pages/index/index.js
@@ -1,9 +1,18 @@
 const app = getApp()
 Page({
   data:{
-
+    imgbtn: []
   },
-  onLoad(){
-    
+  onLoad: function(option) {
+    const self = this
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5cf1f4eaabb0047e81554dd5/starbucks/imgbtn',
+      success (res) {
+        // console.log(res.data)
+        self.setData({
+          imgbtn: res.data.data.imgbtn
+        })
+      }
+    })
   }
 })
